03f9302d48dbf880035f1cfcff4ef73cfa8579c1
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-03-23 21:29:21 +0800

    Update: Login.jsx
    Developed a sign up button component

--- inventory/src/Pages/Login.jsx
+++ inventory/src/Pages/Login.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Input from "../components/Input";
 import Button from "../components/Button";
+import AccountSeperator from "../components/AccountSeperator";
 export default function Login() {
   return (
     <>
-      <div className="text-center">
-        <div className="px-[1em]">
+      <div className="text-center px-[1em]">
+        <div className="">
           <span className="text-center text-3xl">Log in to Y-Inventory</span>
           <Input
             placeholder="sample@email.com"
@@ -16,11 +17,15 @@ export default function Login() {
             editableClassName="h-[5vh] text-blue-500 mt-[1em]"
           />
           <Button
-            customClass="w-full h-[4vh] bg-[#2587be] rounded-[2em] text-white font-[500] text-lg mt-[1.5em]"
+            customClass="w-full h-[6vh] bg-[#2587be] rounded-[2em] text-white font-[500] text-lg mt-[1.5em]"
             buttonName="Submit"
           />
-          
         </div>
+        <AccountSeperator/>
+        <Button
+           customClass="w-1/2 h-[6vh] bg-white rounded-[2em] text-[#2587be] font-[500] text-lg mt-[1em] border-2 border-[#2587be]"
+          buttonName="Sign Up"
+        />
       </div>
     </>
   );
---
+++ inventory/src/components/ErrorMessage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function ErrorMessage(props){
+    return(
+        <>
+            <div>
+                
+            </div>
+        </>
+    )
+}
