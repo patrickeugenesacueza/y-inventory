b09f028c1b9b11a165ba7a186feaad973d54d290
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-03-25 14:41:20 +0800

    Update: App.jsx
    Set-up router of react

--- inventory/src/pages/SignUp.jsx
+++ inventory/src/pages/SignUp.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Input from "../components/Input";
 import Button from "../components/Button";
 import AccountSeperator from "../components/AccountSeperatorSignUp";
+import { Link } from "react-router-dom";
 export default function SignUp() {
   return (
     <>
-      <div className="text-center py-[3em] px-[1em] md:py-[3em] md:px-[10em] lg:px-[20em] xl:px-[35em]">
+      <div className="h-[90vh] text-center py-[10em] px-[1em] md:pb-[1em] md:px-[10em] lg:px-[20em] xl:px-[35em] ">
         <div className="md:border-[1.5px] md:border-[#dcdedf] md:p-[2em] md:rounded-[1em]">
           <span className="text-center text-3xl">Sign up to Y-Inventory</span>
           <Input
@@ -20,16 +21,17 @@ export default function SignUp() {
             placeholder="retype password"
             editableClassName="h-[5vh] text-blue-500 mt-[1em]"
           />
-          <Button
-            customClass="w-full h-[6vh] bg-[#2587be] rounded-[2em] text-white font-[500] text-lg mt-[1.5em]"
-            buttonName="Submit"
-          />
-
-          <AccountSeperator />
-          <Button
-            customClass="w-1/2 h-[6vh] bg-white rounded-[2em] text-[#2587be] font-[500] text-lg mt-[1em] border-2 border-[#2587be]"
-            buttonName="Login"
-          />
+          <Link to="/">
+            <Button
+              customClass="w-full h-[6vh] bg-[#2587be] rounded-[2em] text-white font-[500] text-lg mt-[1.5em]"
+              buttonName="Submit"
+            />
+            <AccountSeperator />
+            <Button
+              customClass="w-1/2 h-[6vh] bg-white rounded-[2em] text-[#2587be] font-[500] text-lg mt-[1em] border-2 border-[#2587be]"
+              buttonName="Sign Up"
+            />
+          </Link>
         </div>
       </div>
     </>
