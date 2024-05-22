84caa0795daf0ab6c6de5c5b61160609b2b33517
Yujin14 <106482110+Yujin14@users.noreply.github.com>
2024-05-22 13:26:50 +0800

    test

--- inventory/src/components/HeaderLogin.jsx
+++ inventory/src/components/HeaderLogin.jsx
@@ -4,7 +4,7 @@ import Icon from "../assets/icon.png";
 export default function NavLogin() {
   return (
     <>
-      <nav className="h-[12vh] w-full px-[1em] fixed bg-white">
+      <nav className="h-[7vh] w-full px-[1em] fixed bg-white">
         <div class="flex h-full w-full justify-center items-center cursor-pointer md:items-start md:justify-start">
           <img src={Icon}/>
         </div>
